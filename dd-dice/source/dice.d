bc34e8bd07c8eec3d3bd10dcb9acc97e3c93ab7d
Olivier Leobal <olivier.contrib@leobal.eu>
2021-05-04 19:25:41 +0200

    Make dice rolls return lists of individual rolls

--- dd-dice/source/dice.d
+++ dd-dice/source/dice.d
@@ -53,24 +53,43 @@ long[] rollDice(long number, long die)
 {
 	return generate!(() => uniform!"[]"(1, die)).takeExactly(number).array;
 }
+bool[] flipCoins(long number)
+{
+	return generate!(() => [true, false].choice).takeExactly(number).array;
+}
 
-/+
- + bools for difficulty rolls (eg d20+5 > 17)
- + strings for name dice and also errors >_>
- +/
 struct ExprResult {
-	Algebraic!(long, bool, string) value;
+	Algebraic!(long, long[], bool, bool[]) value;
 	string repr;
 	this (long   a, string b) { value = a; repr = b; }
+	this (long[] a, string b) { value = a; repr = b; }
 	this (bool   a, string b) { value = a; repr = b; }
-	this (string a, string b) { value = a; repr = b; }
+	this (bool[] a, string b) { value = a; repr = b; }
+	
+	ExprResult reduced() const
+	{
+		if (value.type == typeid(long[]))
+			return ExprResult(value.get!(long[]).sum, repr);
+		if (value.type == typeid(bool[]))
+		{
+			if (value.get!(bool[]).length == 1)
+				return ExprResult(value.get!(bool[])[0], repr);
+			return ExprResult(value.get!(bool[]).sum, repr);
+		}
+		
+		if (value.type == typeid(long))
+			return ExprResult(value.get!long, repr);
+		if (value.type == typeid(bool))
+			return ExprResult(value.get!bool, repr);
+		throw new Exception("Unhandled type: "~value.type.to!string);
+	}
 }
 
 class EvalException : Exception
 {
 	this(string msg, string file = __FILE__, size_t line = __LINE__) {
-        super(msg, file, line);
-    }
+		super(msg, file, line);
+	}
 }
 
 auto eval(string expr)
@@ -86,50 +105,52 @@ ExprResult eval(ParseTree tree)
 	 +/
 	template TypeArithmeticRestriction(string var) {
 		const string TypeArithmeticRestriction =
-			q{if (}~var~q{.value.type != typeid(long) && }~var~q{.value.type != typeid(bool) )
-			throw new EvalException("Can't do arithmetic on "~}~var~q{.value.type.to!string);};
+		q{
+		if (}~var~q{.value.type != typeid(long) && }~var~q{.value.type != typeid(bool) )
+			throw new EvalException("Can't do arithmetic on "~}~var~q{.value.type.to!string);
+		};
 	}
 	switch (tree.name.chompPrefix("DiceExpr."))
 	{
 		case "Comp":
-			auto previousOperand = tree.children[0].eval;
-			auto result = ExprResult(true, previousOperand.repr);
+			auto prevOperand = tree.children[0].eval.reduced;
+			auto result = ExprResult(true, prevOperand.repr);
 			foreach(c;tree.children[1..$])
 			{
 				// Python-style chained comparisons https://docs.python.org/3.8/reference/expressions.html#comparisons
-				auto secondOperand = c.children[0].eval;
+				auto secondOperand = c.children[0].eval.reduced;
 				
-				if (previousOperand.value.type != secondOperand.value.type)
+				if (prevOperand.value.type != secondOperand.value.type)
 					throw new EvalException(
 						"Can't compare values of type "
-						~previousOperand.value.type.to!string
+						~prevOperand.value.type.to!string
 						~" and "~secondOperand.value.type.to!string
 					);
 				bool thisOpResult;
 				if (c.name == "DiceExpr.Eq")
-					thisOpResult = previousOperand.value == secondOperand.value;
+					thisOpResult = prevOperand.value == secondOperand.value;
 				else if (c.name == "DiceExpr.NEq")
-					thisOpResult = previousOperand.value != secondOperand.value;
+					thisOpResult = prevOperand.value != secondOperand.value;
 					
 				else
 				{
-					if (previousOperand.value.type != typeid(long))
+					if (prevOperand.value.type != typeid(long))
 						throw new EvalException(
-							"Can't do other comparisons than equality on "~previousOperand.value.type.to!string
+							"Can't do other comparisons than equality on "~prevOperand.value.type.to!string
 						);
 					switch (c.name.chompPrefix("DiceExpr."))
 					{
 						case "Inf":
-							thisOpResult = previousOperand.value < secondOperand.value;
+							thisOpResult = prevOperand.value < secondOperand.value;
 							break;
 						case "InfEq":
-							thisOpResult = previousOperand.value <= secondOperand.value;
+							thisOpResult = prevOperand.value <= secondOperand.value;
 							break;
 						case "Sup":
-							thisOpResult = previousOperand.value > secondOperand.value;
+							thisOpResult = prevOperand.value > secondOperand.value;
 							break;
 						case "SupEq":
-							thisOpResult = previousOperand.value >= secondOperand.value;
+							thisOpResult = prevOperand.value >= secondOperand.value;
 							break;
 						default:
 							throw new EvalException("Unhandled comparison: "~c.name);
@@ -138,18 +159,19 @@ ExprResult eval(ParseTree tree)
 				}
 				result.value = result.value.get!bool && thisOpResult;
 				result.repr = result.repr ~ c.matches[0] ~secondOperand.repr;
-				previousOperand = secondOperand;
+				prevOperand = secondOperand;
 			}
 			return result;
 		
 		
 		case "Term":
-			auto base = tree.children[0].eval;
+			if (tree.children.length == 1)
+				return tree.children[0].eval;
+			auto base = tree.children[0].eval.reduced;
 			mixin(TypeArithmeticRestriction!"base");
-			
 			foreach(c;tree.children[1..$])
 			{
-				const auto cfactor = c.children[0].eval;
+				const auto cfactor = c.children[0].eval.reduced;
 				mixin(TypeArithmeticRestriction!"cfactor");
 				if (c.name == "DiceExpr.Add")
 				{
@@ -168,11 +190,13 @@ ExprResult eval(ParseTree tree)
 			return base;
 		
 		case "Factor":
-			auto base = tree.children[0].eval;
+			if (tree.children.length == 1)
+				return tree.children[0].eval;
+			auto base = tree.children[0].eval.reduced;
 			mixin(TypeArithmeticRestriction!"base");
 			foreach(c;tree.children[1..$])
 			{
-				const auto cfactor = c.children[0].eval;
+				const auto cfactor = c.children[0].eval.reduced;
 				mixin(TypeArithmeticRestriction!"cfactor");
 				if (c.name == "DiceExpr.Mul")
 				{
@@ -207,20 +231,33 @@ ExprResult eval(ParseTree tree)
 				return ExprResult(0L, "[too many dice]");
 			if (sizeOfDice>99_999_999 ||sizeOfDice<0)
 				return ExprResult(0L, "[dice too large]");
-			if (noOfDice == 0 || sizeOfDice == 0)
-				return ExprResult(0L, "[0]");
+			if (noOfDice == 0)
+				return ExprResult(0, "[0]");
+			
+			
+			if (sizeOfDice <= 2)
+			{
+				bool[] dice;
+				if (sizeOfDice == 0)
+					dice = false.repeat.takeExactly(noOfDice).array;
+				if (sizeOfDice == 1)
+					dice = true.repeat.takeExactly(noOfDice).array;
+				else if (sizeOfDice == 2)
+					dice = flipCoins(noOfDice);
+				return ExprResult(dice, "["~dice.map!(x=>x.to!byte.to!string).join("+")~"]");
+			}
 			
 			auto dice = rollDice(noOfDice, sizeOfDice);
-			return ExprResult(dice.sum, "["~dice.map!(x=>x.to!string).join("+")~"]");
+			return ExprResult(dice, "["~dice.map!(x=>x.to!string).join("+")~"]");
 		
 		case "Neg":
-			auto base = eval(tree.children[0]);
+			auto base = tree.children[0].eval.reduced;
 			base.value = -base.value.get!long;
 			base.repr = "-"~base.repr;
 			return base;
 		
 		case "Not":
-			auto base = eval(tree.children[0]);
+			auto base = tree.children[0].eval.reduced;
 			base.value = !base.value.get!bool;
 			base.repr = "!"~base.repr;
 			return base;
@@ -236,6 +273,7 @@ ExprResult eval(ParseTree tree)
 			return ExprResult(tree.matches[0].to!long, tree.matches[0]);
 		
 		case "DiceExpr":
+			return tree.children[0].eval.reduced;
 		case "Expr":
 		case "Pos":
 		case "Primary":
--- language.md
+++ language.md
@@ -3,37 +3,63 @@
 There is a formal grammar in `dd-dice/source/dice.d` so I won't comment on it.
 However I wrote the intepreter so here's a bit about how it works.
 
-### Type system
+## Dice rolling rules
 
-There are currently two types:
+
+ - `0dy` returns `0`
+ - `xd0` returns a list of `x` failures
+ - `xd1` returns a list of `x` successes
+ - `xd2` return a list of `x` random booleans (coin flips)
+ - `xdy` returns `x` rolls of a `y`-sided die
+
+_Where `x` and `y` are two non-negative integers._ (the grammar is supposed not to allow it)
+
+
+## Type system
+
+There are currently four types:
  - integers (as `long`)
- - booleans
+ - booleans (`true` and `false` in the code, but `Success` and `Failure` in the output)
+ - list of integers
+ - list of bools
 
-Ints represent the result of a roll and bools the result of a comparison.
-However, bools are silently cast to ints when arithmetic is performed, to allow
-for things like counting the number of successes.
+### Lists & ints
 
-I'm also planning on:
- - list of numbers
- - strings
- - list of strings
+Lists represent the result of a roll:
+ - booleans for d0, d1, d2
+ - integers for anything else
 
-Lists of numbers are to represent a roll of multiple dice. At present, rolling
-multiple dice gets you the addition immediately; I would like it to return
-instead a list of numbers that is silently converted to a number by adding them
-either when fed into arithmetic or it reaches the top node. The reason for this
-is that some functions (like "roll 5 take the best 2") actually need the full
-list of rolls.
+Rolls are "reduced" to the sum of their parts when subject to arithmetic or
+comparisons (a roll of 1 bool is reduced to that bool).
+Also a roll of 0 dice returns `0`, not `[0]`.
+
+The reason for preserving the rolls is that some planned functions
+(like "roll 5 take the best 2") actually need the full list of rolls.
 
 We could conceivably have two types of lists of numbers, ones that are silently
 reducibles and ones that aren't. But I think it would be very obscure to the
 point of uselessness.
 
+### Bools
+
+Bools are also the result of a comparison.
+
+Bools are silently cast to ints when arithmetic is performed:
+ - to allow silent arithmetic between coin flips (d2) and other dice
+ - to allow for things like counting the number of successes.
+
+### Planned
+
+I'm also planning on:
+ - strings
+ - list of strings
+
+
 Strings would represent the result of rolling a dice with symbols or words on
 its faces instead of numbers. List of strings for the same (and obviously they
 can't be reducible). There would be a new syntax for this: `2d[head, tails]`
 
-### Functions
+## Functions
 
 The big one: what we need is obviously function composition.
 
@@ -42,7 +68,7 @@ Ideally either prefix (`abs(d10-7)` or infix `(d10-7).abs`)
 The nice thing with function is that everything else can be rewritten as a 
 special case of them.
 
-### Arithmetic division
+## Arithmetic division
 
 Division is currently implemented as `long/long` and therefore floors the result.
 We'd need to sort that out somehow, because "divide, round up" is fairly common.
