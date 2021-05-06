bbd781cdeb091c2a25dfaf3c442fb7afe1446813
Olivier Leobal <olivier.contrib@leobal.eu>
2021-05-06 19:42:28 +0200

    Start work on Lambdas

--- dd-dice/source/dice/interpreter.d
+++ dd-dice/source/dice/interpreter.d
@@ -58,6 +58,93 @@ struct ExprResult {
 	}
 }
 
+
+
+
+
+
+
+string spaces(uint indent)
+{
+	import std.range:repeat;
+	return ' '.repeat(indent*2).to!string;
+}
+
+class Context
+{
+	Context outer;
+	
+	ExprResult[string] contents;
+	
+	ExprResult opIndex(string i)
+	{
+		if (i in contents)
+			return contents[i];
+		else
+		{
+			if (outer)
+				return outer[i];
+			else
+				throw new Exception("undefined in context: "~i.to!string);
+		}
+	}
+	
+	ExprResult opIndexAssign(ExprResult val, string key)
+	{
+		return contents[key] = val;
+	}
+	
+	/++
+	 + will go up to the outer context to change the key at the source
+	 +/
+	ExprResult overwrite(string key, ExprResult val)
+	{
+		if (key in contents)
+			return contents[key] = val;
+		else
+		{
+			if (outer)
+				return outer.overwrite(key, val);
+			else
+				throw new Exception("undefined in context: "~key.to!string);
+		}
+	}
+	
+	this() {}
+	
+	this(Context outer)
+	{
+		this.outer = outer;
+	}
+	
+	
+	override string toString() const
+	{
+		return toString(0);
+	}
+	
+	string toString(uint indent) const
+	{
+		string result = spaces(indent)~"Context(";
+		foreach(k,v;contents)
+			result~="\n"~spaces(indent+1) ~ k ~ " : " ~ v.to!string;
+		if (outer)
+			result~="\n"~outer.toString(indent+1);
+		if (result != spaces(indent)~"Context(")
+			result~="\n";
+		result~=")";
+		return result;
+	}
+}
+
+
+
+
+
+
+
+
+
 class EvalException : Exception
 {
 	this(string msg, string file = __FILE__, size_t line = __LINE__) {
@@ -71,7 +158,7 @@ auto eval(string expr)
 }
 
 
-ExprResult eval(ParseTree tree)
+ExprResult eval(ParseTree tree, Context=new Context())
 {
 	/+ https://github.com/PhilippeSigaud/Pegged/wiki/Generating-Code
 	 + probably a more clever way to do it but I'm too stupid right now
@@ -221,6 +308,9 @@ ExprResult eval(ParseTree tree)
 				tree.name.chompPrefix("DiceExpr.")
 			);
 		
+		
+		//case "LambdaDef":
+			
 		
 		
 		case "MulDie":
--- dd-dice/source/dice/parser.d
+++ dd-dice/source/dice/parser.d
@@ -4,44 +4,49 @@ public import pegged.grammar;
 
 mixin(grammar(`
 DiceExpr:
-	FullExpr < Expr :EndOfInput
-	Expr     < Comp / Term
+	FullExpr   < Expr :EndOfInput
+	Expr       < Comp / Term
 	
-	Comp     < Term (Eq / NEq / Inf / InfEq / Sup / SupEq)+
+	Comp       < Term (Eq / NEq / Inf / InfEq / Sup / SupEq)+
 	
-	Eq       < "==" Term
-	NEq      < "!=" Term
-	Inf      < "<"  Term
-	InfEq    < "<=" Term
-	Sup      < ">"  Term
-	SupEq    < ">=" Term
+	Eq         < "==" Term
+	NEq        < "!=" Term
+	Inf        < "<"  Term
+	InfEq      < "<=" Term
+	Sup        < ">"  Term
+	SupEq      < ">=" Term
 	
 	
-	Term     < Factor (Add / Sub)*
-	Add      < "+" Factor
-	Sub      < "-" Factor
-	Factor   < Primary (Mul / Div)*
-	Mul      < "*" Primary
-	Div      < "/" Primary
-	Primary  < DotCall / FunCall / MulDie / Parens / Not / Neg / Pos / Die / PictDie / Number 
-	Parens   < "(" Expr ")"
-	Not      < "!" Primary
-	Neg      < "-" Primary
-	Pos      < "+" Primary
-	MulDie   < Primary (Die / PictDie)
-	Die      < "d"i Number
-	PictDie  < "d"i "[" UnqStr ("," UnqStr )* :(",")? "]"
-	Number   < ~([0-9]+)
+	Term       < Factor (Add / Sub)*
+	Add        < "+" Factor
+	Sub        < "-" Factor
+	Factor     < Primary (Mul / Div)*
+	Mul        < "*" Primary
+	Div        < "/" Primary
+	Primary    < DotCall / FunCall / MulDie
+	             / Parens / Not / Neg / Pos
+	             / Die / PictDie / Number
+	             / LambdaDef / Ident
+	Parens     < "(" Expr ")"
+	Not        < "!" Primary
+	Neg        < "-" Primary
+	Pos        < "+" Primary
+	MulDie     < Primary (Die / PictDie)
+	Die        < "d"i Number
+	PictDie    < "d"i "[" UnqStr ("," UnqStr )* :(",")? "]"
+	Number     < ~([0-9]+)
 	
-	DotCall  < Primary "." Variable ( "(" ( Expr ("," Expr )* :(",")? )? ")" )?
-	FunCall  < Variable "(" ( Expr ("," Expr )* :(",")? )? ")"
+	DotCall    < Primary "." Ident ( "(" ( Expr ("," Expr )* :(",")? )? ")" )?
+	FunCall    < Ident "(" ( Expr ("," Expr )* :(",")? )? ")"
+	LambdaDef  < Ident ("," Ident )* :(",")? "=>" Expr
 	
-	Variable < identifier
-	UnqStr   <~ String / [a-zA-Z0-9]+
-	String   <~ :doublequote (!doublequote DQChar)* :doublequote / :quote (!quote SQChar)* :quote
-	DQChar   <~ :backslash (doublequote / backslash) / .
-	SQChar   <~ :backslash (quote / backslash) / .
-	EndOfInput  <- !.
+	
+	Ident      < identifier
+	UnqStr     <~ String / [a-zA-Z0-9]+
+	String     <~ :doublequote (!doublequote DQChar)* :doublequote / :quote (!quote SQChar)* :quote
+	DQChar     <~ :backslash (doublequote / backslash) / .
+	SQChar     <~ :backslash (quote / backslash) / .
+	EndOfInput <- !.
 `));
 
 
